a45b3786dcff88f06462acb2fd74132f32ea1579
Johannes Wachter <johannes.wachter@massiveart.com>
2017-11-26 13:20:23 +0100

    added docker environment

---
+++ docker/mysql/Dockerfile
@@ -0,0 +1,2 @@
+FROM mysql
+
--- tests/app/autoload.php
+++ tests/app/autoload.php
@@ -2,10 +2,14 @@
 
 use Composer\Autoload\ClassLoader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
+use Symfony\Component\Dotenv\Dotenv;
 
 /** @var ClassLoader $loader */
 $loader = require __DIR__ . '/../../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+
+$dotenv = new Dotenv();
+$dotenv->load(getcwd() . '/.env');
 
 return $loader;
